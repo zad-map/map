4e90113d0e1331fc0a85c7b2a9b56bcf915c2388
Eric Brelsford <ebrelsford@gmail.com>
2020-01-08 15:16:48 -0500

    Start in 2015

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
 
 export const MINIMUM_DATE = moment('2000-01-01')
-export const START_DATE = moment('2010-01-01')
+export const START_DATE = moment('2015-01-01')
 export const VIDEO_POINTS_FILE = 'https://docs.google.com/spreadsheets/d/1JUccBmHZ2EkA5tNhmpj6fAUaOxU0dyg1eGITTYqYuiw/edit#gid=0';
